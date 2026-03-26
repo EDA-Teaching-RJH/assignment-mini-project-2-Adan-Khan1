55f8951f1ba7f1cd365eb8be6912dca2fff53c49
Adan-Khan1 <ak139@kent.ac.uk>
2026-03-26 00:04:54 +0000

    Created the GUI window and formated the windown visually

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import threading
 import customtkinter as ctk 
 from tkinter import filedialog, messagebox 
 try: 
-    from tkinterdnd2 import tkinterDnD, DND_FILES
+    from tkinterdnd2 import TkinterDnD, DND_FILES
 except ImportError: 
     messagebox.showerror("Missing Dependency", "Please run: pip install tkinterdnd2") # need to pip install tkinterdnd2 and customtkinter. Since these are custom libraries and github do not support the GUI for it, these will be manually shown during the video on local computer
     exit()
@@ -23,3 +23,35 @@ COLOR_ACCENT = "#2E71FA"
 COLOR_SUCCESS = "#3FB950"
 COLOR_WARN = "#D29922"
 COLOR_DANGER = "#F85149"
+
+# Base wrappe to enable native drag and drop on customTkinter since drag and drop functionality is better UX
+class TkinterDnD_CTk(ctk.CTk, TkinterDnD.DnDWrapper):
+    def __init__(self, *args, **kwargs):  # *args is used for the position arguments while **kwargs is used for the keyword arugment for the DnD feature
+        super().__init__(*args, **kwargs)
+        self.TkdndVersion = TkinterDnD._require(self)
+
+class EligibillApp(TkinterDnD_CTk):
+    def __init__(self): 
+        super().__init__()
+
+        self.title("Eligibill Pipeline")
+        self.geometry("1000x800")
+        self.minsize(900,700)
+        self.configure(fg_color=BG_MAIN)
+        ctk.set_appearance_model("dark")
+
+        self.input_filepath = None
+        self.processed_patients = []
+        self.metrics = {"total": 0, "excluded": 0, "manual": 0}
+        self.frames = {}
+
+        self.build_login_screen() 
+        self.build_dashboard()
+
+        self.show_frame("login")
+
+    def show_frame(self, name): 
+        for frame in self.frames.values(): 
+            frame.pack_forget()
+        self.frames[name].pack(fill="both", expand=True)
+
